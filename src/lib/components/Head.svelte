aa0f86ffe7ba30b9659faa248bdd4f85e636a027
Allen <64094914+allendema@users.noreply.github.com>
2025-08-14 23:02:31 +0200

    fix(components): prevent double entries in <header>

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -1,11 +1,14 @@
 <svelte:head>
     <!-- https://webdev.bryanhogan.com/seo/head/#head-code-example -->
 
+    <!-- Already set in app.html
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.png">
+    -->
+
     <title>Zack-Market</title>
     <meta name="description" content="Find the best deals!" />
-    <link rel="icon" href="/favicon.png">
 
     <!-- OpenGraph -->
     <meta property="og:title" content="Zack-Market - Find the best deals!" />
