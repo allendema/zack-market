03561296cacee17bbf46986c220bfcdea7c998c8
Allen <64094914+allendema@users.noreply.github.com>
2025-08-14 18:55:09 +0200

    build: adjust config file for partial svelte/typescript usage

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,38 @@
-// error: https://search.dismail.de/search?q=bun%20svelte%20Uncaught%20%28in%20promise%29%20TypeError%3A%20error%20loading%20dynamically%20imported%20module%3A%20http%3A//localhost%3A5173/node_modules/%40sveltejs/kit/src/runtime/client/entry.js%20&language=auto&time_range=&safesearch=0&categories=general
-//import adapter from "svelte-adapter-bun";
+// import adapter from "svelte-adapter-bun";
+// error: Uncaught (in promise) TypeError: error loading dynamically imported module
+
 import adapter from "@sveltejs/adapter-auto";
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from 'tailwindcss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    // To use non-type-only TypeScript features within Svelte components
+    // https://svelte.dev/docs/svelte/typescript#Preprocessor-setup
+    // this results on error on some dynamic paths
+    //preprocess: vitePreprocess({ script: true }),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter()
+    },
 
-	postcss: {
+    postcss: {
         plugins: [tailwindcss]
+    },
+
+    compilerOptions: {
+        // currently only in one file needed (about.svelte)?
+        experimental: {
+            async: true
     }
+  },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-//import tailwindcss from './node_modules/tailwindcss/vite/dist/index.mjs';
-//import tailwindcss from './node_modules/tailwindcss/dist/plugin.d.ts';
-
 import flowbitePlugin from 'flowbite/plugin'
 
 export default defineConfig({
