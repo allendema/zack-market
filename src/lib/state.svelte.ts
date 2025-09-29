862327a01b6cc763032ca72b374ce775af9c85a2
Allen <64094914+allendema@users.noreply.github.com>
2025-09-29 11:38:20 +0200

    chore(project)!: update listingTypes and use schema for hostnames
    Signed-off-by: Allen <64094914+allendema@users.noreply.github.com>

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,6 +1,12 @@
 // https://svelte.dev/docs/svelte/stores#When-to-use-stores
 
-export const projectName = "Zack-Market";
-export const stores = $state(["Lidl", "Rewe", "Geizhals", "Alnatura"]);
-export const listingTypes = $state(["deals", "brands", "categories","stores"]);
-export const cdnHostnames = $state(["rewe.com", "img.rewe-static.de", "lidl.de", "www.lidl.de", "imgproxy-retcat.assets.schwarz", "geizhals.de", "geizhals.at", "mediaservice.alnatura.de"]);
+export const projectName: string = "Zack-Market";
+export const stores: string[] = $state(["Lidl", "Rewe", "Geizhals", "Alnatura"]);
+export const listingTypes: string[] = $state(["deals", "brands", "categories"]); // TODO: reconsider "stores"
+export const cdnHostnames: string[] = $state(["https://rewe.com", "https://img.rewe-static.de",
+                                             "https://lidl.de", "https://www.lidl.de", "https://imgproxy-retcat.assets.schwarz",
+                                             "https://geizhals.de", "https://gzhls.at",
+                                             "https://mediaservice.alnatura.de",
+                                             "https://cdn.idealo.com"
+                                             ]);
+export const api: string = $state("http://127.0.0.1:8000");
